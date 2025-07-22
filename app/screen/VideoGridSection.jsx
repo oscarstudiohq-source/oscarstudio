2e953937d15f9c1a8025933f78a42f64ebe17ea3
akashbajaj777 <akashbajaj777@gmail.com>
2025-07-22 19:35:48 +0530

    version 1.15.21 -ui fix in video categories

--- app/screen/VideoGridSection.jsx
+++ app/screen/VideoGridSection.jsx
@@ -90,7 +90,8 @@ const VideoCategoryGridSection = () => {
                 </p>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 sm:gap-4 md:gap-5">
+
                 {categories.map((category, index) => (
                     <div
                         key={index}
