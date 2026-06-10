a21da13bbb38987484ca36a69be23a58141a8a19
akashbajaj777 <akashbajaj777@gmail.com>
2026-06-11 01:09:14 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,6 @@ export default function Home() {
         </div>
       </a>
 
-
       <Header />
 
       <main id="orderSection" className="bg-white text-gray-900 py-8 sm:py-10 md:py-8 lg:py-6">
